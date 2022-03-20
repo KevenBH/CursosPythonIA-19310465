3f570a00d7bc7c5f49adf34ab6fbd7ea77462973
KevenBH <a19310465@ceti.mx>
2022-03-19 21:40:27 -0600

    Create Capitulo 4 - Concatenar - Strings 2.py

---
+++ Capitulo 4 - Concatenar - Strings 2.py
@@ -0,0 +1,26 @@
+frase1 = "Hola " + "Mundo"
+print(frase1)
+
+frase2 = "Mi mama "
+frase3 = "me mima"
+frase4 = frase2 + frase3
+print(frase4)
+
+frase5 = "Hola "
+print(frase5 + "Mundo")
+
+nombre = "Kevin"
+apellido = "Benitez"
+apellido2 = "Hernandez"
+nombrecom = nombre + " " + apellido + " " + apellido2
+print(nombrecom)
+
+num1 = "30"
+num2 = "39"
+num3 = num1 + num2
+print(num3)
+
+num = "24" + "35"
+print(num)
+
+print("80" + "12")
