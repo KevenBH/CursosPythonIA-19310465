307971a801393df94a7614bf266254082db368b1
KevenBH <a19310465@ceti.mx>
2022-03-20 00:50:28 -0600

    Create Capitulo 5 - upper(), lower() y title - Strings 3.py

---
+++ Capitulo 5 - upper(), lower() y title - Strings 3.py
@@ -0,0 +1,7 @@
+name1 = "enrique barros fernández".title()
+name2 = "Esta Es Una Frase Para Ser Formateada.".lower()
+name3 = "Esta Es Una Frase Para Ser Formateada.".upper()
+
+print(name1)
+print(name2)
+print(name3)
