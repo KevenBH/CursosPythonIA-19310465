be5e90d72b266a3372cf55c1dc48d0bbd873eed6
KevenBH <a19310465@ceti.mx>
2022-03-20 20:49:21 -0600

    Create Capitulo 7 - Suma, resta, multiplicación y división - Operadores1.py

---
+++ Capitulo 7 - Suma, resta, multiplicación y división - Operadores1.py
@@ -0,0 +1,10 @@
+suma = 20 + 23 + 22 + 22
+resta = 20 - 23 - 34 - 50
+multiplicacion = 15 * 58
+division = 480 / 24 / 2
+operacion = 10 / 5 + 15 - 17
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(operacion)
