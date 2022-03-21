dfc102290ae286a38d8a696c499263d49e6c0273
KevenBH <a19310465@ceti.mx>
2022-03-20 20:49:09 -0600

    Create Capitulo10 - Cómo crear listas y utilizarlas - Listas 1.py

---
+++ Capitulo10 - Cómo crear listas y utilizarlas - Listas 1.py
@@ -0,0 +1,7 @@
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[3])
+print(colores[0])
+print(colores[7])
+
+numeros = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(numeros)
