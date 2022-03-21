4e0c1af2060ebbc0be04a94c4a8f9c9a42ffb0a2
KevenBH <a19310465@ceti.mx>
2022-03-20 23:12:21 -0600

    Create Capitulo 21 - El condicional IF y operadores de comparación.py

---
+++ Capitulo 21 - El condicional IF y operadores de comparación.py
@@ -0,0 +1,17 @@
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+    print('Hola Mundo')
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Hola Mundo 2')
+
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Hola Mundo 3')
